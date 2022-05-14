af603a0f2ab4c089b60ec64fb09294778dbf97f3
Felix Huang <felixhuang12@gmail.com>
2022-05-14 16:37:33 -0400

    axios example

--- part2/example/src/App.js
+++ part2/example/src/App.js
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note' // location given in relation to importing file (e.g. App.js in this case)
 // https://fullstackopen.com/en/part1/component_state_event_handlers#destructuring
+
+
 const App = (props) => { 
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note')
   const [showAll, setShowAll] = useState(true)
-  
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+//output ^ ::
+//render 0 notes // printed first time component is rendered
+// effect // body of function useEffect is immediately executed after rendering
+// promise fulfilled // printed after data arrives from server
+// render 3 notes // setNotes() updates state of notes -> triggers rerendering of component
+
   // addNote is event handler
   const addNote = (event) => {
     event.preventDefault() // prevents default action of submitting a form; page would otherwise reload among other things
--- part2/example/src/index.js
+++ part2/example/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
-
 const notes = [
   {
     id: 1,
@@ -23,6 +23,30 @@ const notes = [
     important: true
   }
 ]
+
+//long hand - storing promise in variable is unnecessary
+// const promise = axios.get('http://localhost:3001/notes')
+// promise.then(response => {
+//   console.log(response)
+// })
+
+//short hand
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data
+//     console.log(response)
+//     console.log(notes)
+// })
+
+// not recommended -> move fetching to App.js
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+// })
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App notes={notes} />
